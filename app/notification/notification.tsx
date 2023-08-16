b3060b9fb421f20b8c7ad9ff392110a89d9f1c87
Dewantoro Triatmojo <dewantorotriatmojo@gmail.com>
2023-08-16 18:58:25 +0700

    feat: Created notification page.

---
+++ app/notification/notification.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { getTimePassed } from "@/lib/utils";
+import Link from "next/link";
+
+interface NotificationProps {
+  id: string;
+  title: string;
+  description: string;
+  isSeen: boolean;
+  date: Date;
+  redirectLink: string;
+}
+
+const Notification = ({
+  id,
+  title,
+  description,
+  isSeen,
+  date,
+  redirectLink,
+}: NotificationProps) => {
+  const handleClick = () => {
+    // Update database notification to be seen
+  };
+
+  return (
+    <Link href={redirectLink}>
+      <li
+        key={id}
+        tabIndex={0}
+        className={`cursor-pointer border border-border p-3 ${
+          isSeen ? "bg-background" : "bg-muted"
+        } `}
+        onClick={handleClick}
+      >
+        <h2 className="text-base font-semibold">{title}</h2>
+        <p className="text-sm">{description}</p>
+        <p className="text-xs">{getTimePassed(date)}</p>
+      </li>
+    </Link>
+  );
+};
+
+export default Notification;
