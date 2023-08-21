96a1819e65228816a4ff70ff6339b40f9acceab8
AustinPardosi <gabrielpardosi26@gmail.com>
2023-08-21 19:03:05 +0700

    feat: add main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import HomePage from "@/components/home-page";
 
 export default function Home() {
   return (
     <main className="flex min-h-[200vh]">
-      <h1>Home Page</h1>
+      <HomePage/>
     </main>
   );
 }
