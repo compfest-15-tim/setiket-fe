9824d889fd8ab2dfd0cfa722d2a4c47c1f173534
AustinPardosi <gabrielpardosi26@gmail.com>
2023-08-21 19:03:05 +0700

    feat: add main page

---
+++ components/mockActivity.tsx
@@ -0,0 +1,26 @@
+export const activities = [
+    {
+        id: 1,
+        name: "Universal Studios Singapore",
+        location: "Pulau Sentosa, Singapura",
+        rating: "4,5/5",
+        price: 788.540,
+        img: "https://images.unsplash.com/photo-1578400742228-b18c424361e7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=701&q=80",
+    },
+    {
+        id: 2,
+        name: "S.E.A Aquarium & Van Gogh: The Immersive Experience",
+        location: "Pulau Sentosa, Singapura",
+        rating: "4,8/5",
+        price: 775.023,
+        img: "https://images.unsplash.com/photo-1501561869-f59c248fab54?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
+    },
+    {
+        id: 3,
+        name: "Gardens by the Bay",
+        location: "Marina South, Singapura",
+        rating: "4,7/5",
+        price: 112.649,
+        img: "https://images.unsplash.com/photo-1506351421178-63b52a2d2562?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1332&q=80",
+    },
+];
