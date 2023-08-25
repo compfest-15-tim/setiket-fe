c36e4827b30aa241afb515f39e94cd9a2c5dca17
henryhenn <henrysalim22@gmail.com>
2023-08-25 20:59:19 +0700

    feat: Created homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,48 @@
+import { Metadata } from "next";
+import Link from "next/link";
+import { MoveRight } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import EventCards from "@/components/event-cards";
+
+export const metadata: Metadata = {
+  title: "Home | SeTiket",
+};
+
 export default function Home() {
   const arr = [1, 2, 3, 4, 5];
   return (
-    <main className="flex min-h-[200vh]">
-      <h1>
-        Home Page<p>TES</p>
-      </h1>
-      <h2>
-        TES<p>TES</p>
-      </h2>
-      <p>TES</p>
-      <p>TES</p>
-    </main>
+      <main className="flex flex-auto flex-col justify-center bg-gray-100 bg-[url('/homepage-2.jpg')] bg-cover p-5 text-center text-white md:p-12">
+        <div>
+          <h1 className="text-5xl font-bold lg:text-6xl">
+            Welcome to{" "}
+            <span className="font-extrabold text-primary">SeTiket!</span>
+          </h1>
+
+          <p className="mt-6 text-lg md:text-xl">
+            Welcome to <span className="font-bold text-primary">SeTiket</span>,
+            where you can find & buy awesome events's ticket!
+          </p>
+
+          <Link
+            href="/events"
+            className="mt-10 block font-semibold text-primary hover:underline"
+          >
+            <Button className="inline-flex gap-1" size="lg">
+              See all events here
+              <MoveRight />
+            </Button>
+          </Link>
+        </div>
+
+        <div className="mt-32">
+          <h2 className="text-4xl font-bold lg:text-5xl">
+            Latest <span className="text-primary">Events</span>
+          </h2>
+
+          <div className="mt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+            <EventCards />
+          </div>
+        </div>
+      </main>
   );
 }
