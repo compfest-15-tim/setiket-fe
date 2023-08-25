68f9d5add8b0c758b735dc5674d67a56804f6bd9
Dewantoro Triatmojo <dewantorotriatmojo@gmail.com>
2023-08-26 01:25:09 +0700

    fix: production api endpoint undefined url.

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,5 +1,5 @@
 // Backend API Base URL
 export const BASE_URL =
   process.env.NODE_ENV === "production"
-    ? process.env.API_URL
+    ? process.env.NEXT_PUBLIC_API_URL
     : "http://localhost:8000";
