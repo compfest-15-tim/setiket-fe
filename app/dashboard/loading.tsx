b3a6998a3d0bea384b3d81ff74c78c746fd38955
Lint Action <dewantorotriatmojo@gmail.com>
2023-08-26 13:33:19 +0000

    Fix code style issues with Prettier

--- app/dashboard/loading.tsx
+++ app/dashboard/loading.tsx
@@ -1,7 +1,7 @@
 export default function Loading() {
   return (
-    <main className="flex flex-auto flex-col align-middle justify-center text-center">
-      <p className="font-semibold text-xl">Loading...</p>
+    <main className="flex flex-auto flex-col justify-center text-center align-middle">
+      <p className="text-xl font-semibold">Loading...</p>
     </main>
-  )
+  );
 }
